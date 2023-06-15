3de21dbff3d78b3d7d6a54b711a508da6376b886
Joel Johnson <60250054+jj-tech-io@users.noreply.github.com>
2023-06-15 13:56:31 -0700

    docker ae deployment

---
+++ load_modules.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow.python.keras import layers
+from tensorflow.python.keras.layers import Dense, Flatten, Input
+from tensorflow.python.keras.models import Model, Sequential, load_model, save_model
+from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, LambdaCallback
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import PIL.Image as Image
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import time
+from tensorflow.python.keras.optimizers import adam_v2 as Adam
+from tensorflow.python.keras.layers import Input, Dense
+from tensorflow.python.keras.models import Model
+import tensorflow.python.keras.backend as K
+from tensorflow.python.client import device_lib
+import os 
+import datetime
+# import colorspacious as cs
+# from colorspacious import cspace_convert
+from tensorflow.keras.layers import Lambda, BatchNormalization
+from tensorflow.python.client import device_lib
