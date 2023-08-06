eda975188ce9cedb92a003dcd8927d6a4d2a4fc3
Joel Johnson <60250054+jj-tech-io@users.noreply.github.com>
2023-08-06 19:10:48 +0200

    rome ap

--- my_config.py
+++ my_config.py
@@ -1,13 +1,13 @@
-USE_GPU = True
-LOCAL_SAVE = r"C:\Users\joeli\Dropbox\TopResultlsOut"
+USE_GPU = False
+# LOCAL_SAVE = r"C:\Users\joeli\Dropbox\TopResultlsOut"
 RUN_LOCAL = True
 IMAGE_FOLDER = None
 ENCODER_PATH = None
 DECODER_PATH = None
 if RUN_LOCAL:
     IMAGE_FOLDER = r"models_4k"
-    ENCODER_PATH = r"C:\Users\joeli\OneDrive\Desktop\AE_2023_06\TrainedModels\no_duplicates_75_2_mask\encoder.h5"
-    DECODER_PATH = r"C:\Users\joeli\OneDrive\Desktop\AE_2023_06\TrainedModels\no_duplicates_75_2_mask\decoder.h5"
+    ENCODER_PATH = r"/TrainedModels/316/encoder.h5"
+    DECODER_PATH = r"/TrainedModels/316/decoder.h5"
     # "C:\Users\joeli\OneDrive\Desktop\AE_2023_06\TrainedModels\no_duplicates_75_2_mask\no_duplicates_75_2_maskdecoder.h5"
 
 else:
@@ -16,8 +16,8 @@ else:
     # DECODER_PATH = r"/AE_2023_06/TrainedModels/316/decoder.h5"
     #TrainedModels\no_duplicates_75_2_mask\encoder.h5
     #TrainedModels/no_duplicates_75_2_mask/encoder.h5
-    ENCODER_PATH = r"/AE_2023_06/TrainedModels/no_duplicates_75_2_mask/encoder.h5"
-    DECODER_PATH = r"/AE_2023_06/TrainedModels/no_duplicates_75_2_mask/decoder.h5"
+    ENCODER_PATH = r"TrainedModels/316/encoder.h5"
+    DECODER_PATH = r"TrainedModels/316/decoder.h5"
     
 CMAP_SPECULAR = 'viridis'
 
